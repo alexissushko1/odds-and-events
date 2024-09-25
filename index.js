2a513d0aaef5b0e5df0cf083659d8229cb170532
Alexis Sushko <alexis.sushko@gmail.com>
2024-09-25 17:45:38 -0400

    Update final code to make site functional where the sort all button correctly sorts all of the numbers into the odds and even arrays at one time, not just for 2 numbers. Set .value to an empty string so the number would disappear after submitting it.

--- index.js
+++ index.js
@@ -50,26 +50,44 @@ Array is stored in number bank
 
     //===State===
 
+    /* added 3 arrays, 1 for the numberBank that the number from the text input would go. 1 array was for the numbers
+    that would be moved from the numberBank if they were odd and 1 that moved from the 
+    number bank if they were even*/
+
     const numberBank = [];
     const odds = [];
     const evens = [];
+
+    /* Created a function to get a number,
+    add (push) that number to the numberbank, 
+    and log so it shows in the console */
 
     function addNumber(number) {
         numberBank.push(number);
         console.log(numberBank);
       }  
 
+      /* Created a function that uses shift to take
+      the first number from the numberBank array and sort
+      it so that if it has is a remainder of 1, we know it is 
+      odd and pushed the odds array, otherwise, it pushes
+      to the evens array */
       function sort() {
         const number = numberBank.shift();
         if(number%2===1) {
           odds.push(number);
-        } else {
+        } else{
           evens.push(number);
         }
       }
 
 
       //---Render---
+
+      /* Created a function to render the numbers by
+      setting the dom variable $numbers and assigning
+      it to a number mapped as a div element that shows
+      the number */
 
       const renderNumbers = () => {
         const $numbers = numberBank.map((number) => {
@@ -81,6 +99,12 @@ Array is stored in number bank
         numberInput.replaceChildren(...$numbers);
       }
 
+      /* Created a function to render the odd numbers by
+      setting the dom variable $numbers assigned to the mapped 
+      number that is a created div that displays the number
+      if it is odd */
+
+
       const renderOdds = () => {
         const $numbers = odds.map((number) => {
           const $number = document.createElement("div");
@@ -90,18 +114,69 @@ Array is stored in number bank
         const numberInput = document.querySelector("#odds");
         numberInput.replaceChildren(...$numbers);
       }
+
+         /* Created a function to render the odd numbers by
+      setting the dom variable $numbers assigned to the mapped 
+      number that is a created div that displays the number
+      if it is odd */
+
+      const renderEvens = () => {
+        const $numbers = evens.map((number) => {
+          const $number = document.createElement("div");
+          $number.textContent=number;
+          return $number;
+        });
+        const numberInput = document. querySelector("#evens");
+        numberInput.replaceChildren(...$numbers);
+      }
+
+      //Created a function to hold all of the render functions
+     
       function render(){
         renderOdds();
+        renderEvens();
         renderNumbers();
       }
-     
+
+
+
       //===Script===
 
+      /* Set dom variable $sortOne, got the id from 
+      the HTML and added an event listener to listen for 
+      the click of the first button, then perform the sort and render functions. */
+            
+      const $sortOne = document.querySelector("#sortOne");
+      $sortOne.addEventListener("click", () => {
+        sort();
+        render();
+      })
+
+        /* Set dom variable $sortAll got the id from 
+      the HTML and added an event listener to listen for 
+      the click of the next button, then perform the sort and render functions for 
+      all of the numbers in the numberBank(using a while loop to 
+        check for all of the numbers. */
+
+      const $sortAll = document.querySelector("#sortAll");
+      $sortAll.addEventListener("click", () => {
+        while (0 < numberBank.length) {
+        sort();
+      }
+      render();
+      })
+
+      /* Created a DOM variable that uses querySelector to get the form from
+      the HTML and then apply an event listener to it. The 
+      event listener will wait for submit, set the form to 
+      not refresh after submitted, get the number from the 
+      HTML, perform the addNumber function and then render it. */
       const $form = document.querySelector("form");
       $form.addEventListener("submit", (event) => {
         event.preventDefault();
         const $number = document.querySelector("#number");
         addNumber($number.value);
+        $number.value="";
         render();
       })
       
